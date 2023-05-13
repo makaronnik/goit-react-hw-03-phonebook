77fc7b938e29b32a867ff6ac985f4d080b21cffc
Andriy Karpishyn <makaron4eg@gmail.com>
2023-05-13 17:40:11 +0300

    Task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,30 @@ import Filter from './ContactList/Filter/Filter';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 
+const CONTACTS_STORAGE_KEY = 'contacts';
+
 export class App extends Component {
   state = {
     contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(CONTACTS_STORAGE_KEY);
+
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(
+        CONTACTS_STORAGE_KEY,
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
 
   addContact = ({ name, number }) => {
     if (this.state.contacts.some(contact => contact.name === name)) {
